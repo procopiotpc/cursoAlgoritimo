9d4c6f0f197e755b06d26d74bb09c02c3b3b9353
Tiago Procópio de Carvalho <procopiotpc@gmail.com>
2025-03-14 03:10:10 +0000

    Aula 20250314

---
+++ exercicio01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+    // int, float, double
+    // char, char[]
+
+    char cod;
+    char nome[30];
+    unsigned int pontosTuristicos;
+    float area;
+    double pib;
+
+    return 0;
+}
