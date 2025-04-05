e360d9b2c0d08a40d616fa2bc846670735430494
Tiago Procópio de Carvalho <procopiotpc@gmail.com>
2025-04-05 20:06:20 +0000

    Aula de dados variáveis

---
+++ tiposDeDados.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+
+    //printf("Hello, world! \n");
+
+    int idade = 40;
+    float altura = 1.90;
+    char sigla[30] = "Tiago";
+
+
+    printf ("O %.s tem %d anos de idade e %.2fm de altura.\n",sigla, idade, altura); 
+
+    return 0;
+}
